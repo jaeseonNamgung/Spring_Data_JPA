69ba5bde1fd65fd2bf6a3b97048a8d45c6d93265
jaeseonNamgung <sunnamgung8@naver.com>
2023-02-01 17:29:53 +0900

    @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -90,4 +90,17 @@ class MemberRepositoryTest {
         // then
         assertThat(findMember).isEqualTo(m1);
     }
+    @Test
+    void testdQuery() throws Exception{
+        // given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        // when
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        Member findMember = result.get(0);
+        // then
+        assertThat(findMember).isEqualTo(m1);
+    }
 }
