73269d8ea8f1990be3b4ffd32a6080e639fd3c68
jaeseonNamgung <sunnamgung8@naver.com>
2023-02-01 18:27:56 +0900

    순수  JPA 페이징과 정렬

--- src/test/java/com/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -97,6 +97,29 @@ class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(m1);
     }
 
+    @Test
+    void paging() throws Exception{
+        // given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+        memberJpaRepository.save(new Member("member6", 10));
+        memberJpaRepository.save(new Member("member7", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        // when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+        // then
+        assertThat(members).hasSize(3);
+        assertThat(totalCount).isEqualTo(7);
+    }
+
 
 
 }
